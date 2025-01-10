54ac3c28cc8650c8136b97b7b429a4fa10cc08a8
victorpapuc <Victor.Papuc@external.erstegroup.com>
2025-01-10 09:01:07 +0100

    [Web] Added landing page

--- src/routes/sections/index.tsx
+++ src/routes/sections/index.tsx
@@ -2,16 +2,36 @@ import {Navigate, useRoutes} from 'react-router-dom';
 
 import {PATH_AFTER_LOGIN} from 'src/config-global';
 
-import {mainRoutes} from './main';
+import { HomePage, mainRoutes } from './main';
 import {authRoutes} from './auth';
 import {dashboardRoutes} from './dashboard';
 import {useFeatureFlags} from "../../utils/featureflags";
+import DashboardLayout from "../../layouts/dashboard";
+import MainLayout from "../../layouts/main";
 
 // ----------------------------------------------------------------------
 
 export default function Router() {
   const featureFlags = useFeatureFlags()
   return useRoutes([
+
+    // SET INDEX PAGE WITH SKIP HOME PAGE
+    // {
+    //   path: '/',
+    //   element: <Navigate to={PATH_AFTER_LOGIN} replace />,
+    // },
+
+    // ----------------------------------------------------------------------
+
+    // SET INDEX PAGE WITH HOME PAGE
+    {
+      path: '/',
+      element: (
+        <MainLayout>
+          <HomePage />
+        </MainLayout>
+      ),
+    },
     {
       path: '/',
       element: <Navigate to={PATH_AFTER_LOGIN} replace/>,
