2bc42c85624862d4cf78445133acb0bfb50790f5
victorpapuc <Victor.Papuc@external.erstegroup.com>
2025-01-13 09:00:51 +0100

    [Web] Added landing page1

--- src/layouts/main/header.tsx
+++ src/layouts/main/header.tsx
@@ -2,26 +2,25 @@ import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
 import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
 import Toolbar from '@mui/material/Toolbar';
-import { useTheme } from '@mui/material/styles';
+import {useTheme} from '@mui/material/styles';
 import Container from '@mui/material/Container';
-import Badge, { badgeClasses } from '@mui/material/Badge';
+import Badge, {badgeClasses} from '@mui/material/Badge';
 
-import { paths } from 'src/routes/paths';
+import {paths} from 'src/routes/paths';
 
-import { useOffSetTop } from 'src/hooks/use-off-set-top';
-import { useResponsive } from 'src/hooks/use-responsive';
+import {useOffSetTop} from 'src/hooks/use-off-set-top';
+import {useResponsive} from 'src/hooks/use-responsive';
 
-import { bgBlur } from 'src/theme/css';
+import {bgBlur} from 'src/theme/css';
 
 import Logo from 'src/components/logo';
 import Label from 'src/components/label';
 
 import NavMobile from './nav/mobile';
 import NavDesktop from './nav/desktop';
-import { HEADER } from '../config-layout';
-import { navConfig } from './config-navigation';
+import {HEADER} from '../config-layout';
+import {navConfig} from './config-navigation';
 import LoginButton from '../common/login-button';
 import HeaderShadow from '../common/header-shadow';
 import SettingsButton from '../common/settings-button';
@@ -88,9 +87,9 @@ export default function Header() {
           {mdUp && <NavDesktop data={navConfig} />}
 
           <Stack alignItems="center" direction={{ xs: 'row', md: 'row-reverse' }}>
-            <Button variant="contained" target="_blank" rel="noopener" href={paths.minimalUI}>
-              Purchase Now
-            </Button>
+            {/*<Button variant="contained" target="_blank" rel="noopener" href={paths.minimalUI}>*/}
+            {/*  Purchase Now*/}
+            {/*</Button>*/}
 
             {mdUp && <LoginButton />}
 
